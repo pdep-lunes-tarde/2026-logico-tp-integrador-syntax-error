3486f7af3e90db2048c901d6452aa3d5dfea9ef9
fransegura04 <fran2004segura@gmai.com>
2026-08-01 18:46:04 -0300

    subo punto 2

--- src/tp.pl
+++ src/tp.pl
@@ -30,15 +30,62 @@ estaVivo(Habitante, AnioActual) :-
     habitante(Habitante, elfo, AnioNacimiento, _),
     AnioNacimiento =< AnioActual.
 
-% Punto 2 ()
-% a)
+% Punto 2 (Fran)
+% presencio(Persona, Hazania, AnioEnQuePreseencio, LugarSegunEsaPersona, HeroesSegunEsaPersona)
+presencio(wirbel, rescatarHermanaWirbel, 1390, klares, [strak, fern]).
+presencio(frieren, rescatarHermanaWirbel, 1390, klares, [strak, fern]).
+presencio(kanne, recuperarGatoPerdido, 1375, weise, [himmel, frieren]).
+% escucho(Persona, Hazania, AnioEnQueEscucho, LugarSegunEsaPersona, HeroesSegunEsaPersona)
+escucho(lawine, destruirDemonioAura, 1393, weise, [frieren]).
+% leyo(Persona, Hazania, AnioEnQueLeyo, Paginas, LugarSegunEsaPersona, HeroesSegunEsaPersona)
+leyo(voll, destruirDemonioAura, 1400, 50, auberst, [denek]).
+leyo(serie, destruirReyDemonio, 1335, 100, ende, [frieren, himmel, heiter, eisen]).
+% a) 
+recuerdaHazania(Persona, Hazania, AnioActual) :-
+    presencio(Persona, Hazania, AnioPresencio, _, _),
+    AnioPresencio =< AnioActual,
+    estaVivo(Persona, AnioActual).
 
+recuerdaHazania(Persona, Hazania, AnioActual) :-
+    escucho(Persona, Hazania, AnioEscucho, _, _),
+    AnioActual >= AnioEscucho,
+    AnioActual =< AnioEscucho + 15,
+    estaVivo(Persona, AnioActual).
+
+recuerdaHazania(Persona, Hazania, AnioActual) :-
+    leyo(Persona, Hazania, AnioLeyo, Paginas, _, _),
+    AnioActual >= AnioLeyo,
+    AnioActual =< AnioLeyo + Paginas,
+    estaVivo(Persona, AnioActual).
 
 % b)
+version(Hazania, Lugar, HeroesOrdenados) :-
+    presencio(_, Hazania, _, Lugar, Heroes),
+    sort(Heroes, HeroesOrdenados).
+
+version(Hazania, Lugar, HeroesOrdenados) :-
+    escucho(_, Hazania, _, Lugar, Heroes),
+    sort(Heroes, HeroesOrdenados).
+
+version(Hazania, Lugar, HeroesOrdenados) :-
+    leyo(_, Hazania, _, _, Lugar, Heroes),
+    sort(Heroes, HeroesOrdenados).
+ 
+hazaniaCorroborada(Hazania) :-
+    findall((Lugar, Heroes), version(Hazania, Lugar, Heroes), Versiones),
+    sort(Versiones, VersionesUnicas),
+    length(VersionesUnicas, 1).
+
 
 
 % c)
+conocioHazania(Persona, Hazania) :- presencio(Persona, Hazania, _, _, _).
+conocioHazania(Persona, Hazania) :- escucho(Persona, Hazania, _, _, _).
+conocioHazania(Persona, Hazania) :- leyo(Persona, Hazania, _, _, _, _).
 
+pasoAlOlvido(Hazania, Anio) :-
+    conocioHazania(_, Hazania),
+    \+ ( conocioHazania(Persona, Hazania), recuerdaHazania(Persona, Hazania, Anio) ).
 
 % Punto 3 ()
 % a)
@@ -63,7 +110,28 @@ test("Serie está viva en el año 5000 porque los elfos no mueren de viejos."):-
     estaVivo(serie, 5000).
 
 % Tests Punto 2
-
+test("Lawine no recuerda destruir al demonio Aura en 1380 porque aun no escucho una cancion sobre esa hazana."):-
+    \+ recuerdaHazania(lawine, destruirDemonioAura, 1380).
+test("Lawine recuerda destruir al demonio Aura en 1400", nondet):-
+    recuerdaHazania(lawine, destruirDemonioAura, 1400).
+test("Lawine ya no recuerda destruir al demonio Aura en 1410, porque pasaron mas de 15 anios de que escucho la cancion"):-
+    \+ recuerdaHazania(lawine, destruirDemonioAura, 1410).
+test("Voll recuerda destruir al demonio Aura en 1450", nondet):-
+    recuerdaHazania(voll, destruirDemonioAura, 1450).
+test("Voll no recuerda destruir al demonio Aura en 1460"):-
+    \+ recuerdaHazania(voll, destruirDemonioAura, 1460).
+test("Wirbel recuerda rescatar a la hermana de wirbel en 1430", nondet):-
+    recuerdaHazania(wirbel, rescatarHermanaWirbel, 1430).
+test("Wirbel ya no recuerda rescatar a la hermana de wirbel en 1440 porque no esta vivo en ese anio"):-
+    \+ recuerdaHazania(wirbel, rescatarHermanaWirbel, 1440).
+test("rescatar a la hermana de Wirbel es una hazana corroborada", nondet):-
+    hazaniaCorroborada(rescatarHermanaWirbel).
+test("destruir al demonio Aura no es una hazana corroborada (las diferentes personas que la conocen no estan de acuerdo ni en lugar ni en los heroes que la llevaron a cabo)"):-
+    \+ hazaniaCorroborada(destruirDemonioAura).
+test("destruir al demonio Aura paso al olvido en 1460", nondet):-
+    pasoAlOlvido(destruirDemonioAura, 1460).
+test("destruir al demonio Aura no paso al olvido en 1440"):-
+    \+ pasoAlOlvido(destruirDemonioAura, 1440).
 
 % Tests Punto 3
 
